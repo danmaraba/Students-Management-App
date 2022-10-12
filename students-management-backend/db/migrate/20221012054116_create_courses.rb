a25dda408b682b7c2befe0a300faa899dd5ef1b0
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-10-12 09:20:25 +0300

    Chore(backend):updated the courses table

--- students-management-backend/db/migrate/20221012054116_create_courses.rb
+++ students-management-backend/db/migrate/20221012054116_create_courses.rb
@@ -2,8 +2,8 @@ class CreateCourses < ActiveRecord::Migration[7.0]
   def change
     create_table :courses do |t|
       t.string :department
-      t.integer :tutor_id
       t.integer :student_id
+      t.integer :tutor_id
 
       t.timestamps
     end
