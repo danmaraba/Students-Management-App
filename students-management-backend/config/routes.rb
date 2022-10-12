3c11f420dfa35d2769a1f522073dfb5633d6a4ac
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-10-12 10:20:49 +0300

    Chore(backend):added create and destroy actions to students route

--- students-management-backend/config/routes.rb
+++ students-management-backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :students, only:[:index,:show]
+  resources :students, only:[:index,:show,:create,:destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
