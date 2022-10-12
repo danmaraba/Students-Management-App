9390ca1f80e9d7afc14110cad22d20f484c26590
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-10-12 10:19:37 +0300

    Chore(backend):created create and destroy methods

--- students-management-backend/app/controllers/students_controller.rb
+++ students-management-backend/app/controllers/students_controller.rb
@@ -1,18 +1,34 @@
 class StudentsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_not_valid_response
     def index
         students = Student.all
         render json: students, status: :ok
     end
     def show
-        students =find_students
+        students =find_student
         render json: students
     end
+    def create
+        student = Student.create!(students_params)
+        render json: student, status: :created
+    end
+    def destroy
+        student = find_student
+        student.destroy
+        head :no_content
+    end
     private
-    def find_students
+    def find_student
         Student.find(params[:id])
     end
+    def students_params
+        params.permit(:name,:gender,:course,:year_of_study)
+    end
     def render_not_found_response
-    render json: { error: "Student not found" }, status: :not_found
+        render json: { error: "Student not found" }, status: :not_found
+    end
+    def render_not_valid_response
+        render json: { error: "Invalidation errors" }, status: :not_found
     end
 end
