7c1b8a790e985e518d571df9f140ef904bd28de1
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-10-12 12:45:14 +0300

    Chore(backend):updated class student

--- students-management-backend/app/models/student.rb
+++ students-management-backend/app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ApplicationRecord
     has_many :courses
-    has_many :teachers,through: :courses
+    has_many :tutors,through: :courses
     validates :name, presence:true
     validates :course, presence:true
 end
