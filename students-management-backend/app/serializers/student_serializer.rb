d477c3d77fa84055e0cd45bdd2f78aad9444112e
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-10-13 05:41:48 +0300

    Chore(backend):updated student serializer

--- students-management-backend/app/serializers/student_serializer.rb
+++ students-management-backend/app/serializers/student_serializer.rb
@@ -1,4 +1,4 @@
 class StudentSerializer < ActiveModel::Serializer
-  attributes :name,:gender,:course,:year_of_study
+  attributes :name,:gender,:course,:year_of_study,:email,:username,:password_digest
   has_many :tutors
 end
