43e50a509fc6d44f9e900d892ce7488cfaa400b9
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-10-13 05:42:27 +0300

    Chore(backend):updated the schema

--- students-management-backend/db/schema.rb
+++ students-management-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_12_054116) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_12_131746) do
   create_table "courses", force: :cascade do |t|
     t.string "department"
     t.integer "student_id"
@@ -26,6 +26,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_12_054116) do
     t.integer "year_of_study"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "password_digest"
+    t.string "email"
   end
 
   create_table "tutors", force: :cascade do |t|
